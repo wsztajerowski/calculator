36acb66e57f7a32abdb1f03a600a967f64a4f6c6
Wiktor Sztajerowski <wiktor.sztajerowski@gmail.com>
2020-11-08 19:48:52 +0100

    object calculator tests

--- src/test/java/pl/symentis/calculator/ExtendedObjectCalculatorAssert.java
+++ src/test/java/pl/symentis/calculator/ExtendedObjectCalculatorAssert.java
@@ -3,8 +3,9 @@ package pl.symentis.calculator;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Assertions;
 
-public class ExtendedObjectCalculatorAssert {
+public class ExtendedObjectCalculatorAssert extends AbstractAssert<ExtendedObjectCalculatorAssert, ObjectCalculator> {
     public ExtendedObjectCalculatorAssert(ObjectCalculator actual) {
+        super(actual, ExtendedObjectCalculatorAssert.class);
     }
 
     public static ExtendedObjectCalculatorAssert assertThat(ObjectCalculator actual) {
@@ -12,11 +13,21 @@ public class ExtendedObjectCalculatorAssert {
     }
 
     public ExtendedObjectCalculatorAssert hasValueDifferentThan(int value) {
+        isNotNull();
+
+        if (actual.getCurrentValue() == value) {
+            failWithMessage("Expected calculator current value to be different than <%s> but was the same", value);
+        }
 
         return this;
     }
 
     public ExtendedObjectCalculatorAssert hasPositiveValue() {
+        isNotNull();
+
+        Assertions.assertThat(actual.getCurrentValue())
+                .overridingErrorMessage("Current value [%s] has to be a positive number!", actual.getCurrentValue())
+                .isPositive();
         return this;
     }
 }
--- src/test/java/pl/symentis/calculator/ObjectCalculatorIT.java
+++ src/test/java/pl/symentis/calculator/ObjectCalculatorIT.java
@@ -17,13 +17,55 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class ObjectCalculatorIT {
     private ObjectCalculator sut;
 
+    @Test
     void add_one_to_three_returns_four(){
+        // given
+        sut = new ObjectCalculator();
+
+        // when
+        int actual = sut
+                .init(1)
+                .add(3)
+                .getCurrentValue();
+
+        // then
+        assertThat(actual)
+                .isPositive()
+                .isEqualTo(4);
     }
 
+    @ParameterizedTest(name = "Divide {0} by 0 throw an exception!")
+    @ValueSource(ints = {-1, 0, 1, 4})
     void throws_illegal_argument_exception_when_dividing_by_zero(int value){
+        // given
+        sut = new ObjectCalculator(value);
+
+        // when
+        Throwable thrown = catchThrowable(() -> sut.divideBy(0));
+
+        // then
+        assertThat(thrown)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("by zero!")
+            .hasNoCause();
     }
 
+    @ParameterizedTest(name = "Result of multiplication [{arguments}] is positive")
+    @DisplayName(value = "Multiplication numbers with same sign returns positive result")
+    @MethodSource("multiplicationElements")
     void multiplication_of_two_numbers_with_same_sign_returns_positive_number(int firstNumber, int secondNumber){
+        // given
+        sut = new ObjectCalculator();
+
+        // when
+        ObjectCalculator actual = sut
+            .init(firstNumber)
+            .multiplyBy(secondNumber);
+
+        // then
+        ExtendedObjectCalculatorAssert.assertThat(actual)
+            .hasValueDifferentThan(0)
+            .hasPositiveValue();
     }
 
 
